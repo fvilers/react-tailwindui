94eeb80f9544d965d5aa3fdc0d2d49fe1646e05e
Fabian Vilers <fabian.vilers@dev-one.com>
2024-06-14 13:40:18 +0200

    Add the react router

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,12 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "perfectionist"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "perfectionist/sort-objects": "error",
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./home/HomePage";
 
+const router = createBrowserRouter([{ element: <HomePage />, path: "/" }]);
+
 function App() {
-  return <HomePage />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
