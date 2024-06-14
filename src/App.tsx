548537ebbca383d1d04f07a5983009e0c066c66a
Fabian Vilers <fabian.vilers@dev-one.com>
2024-06-14 13:41:18 +0200

    Add the not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./home/HomePage";
+import NotFoundPage from "./not-found/NotFoundPage";
 
-const router = createBrowserRouter([{ element: <HomePage />, path: "/" }]);
+const router = createBrowserRouter([
+  { element: <HomePage />, path: "/" },
+  { element: <NotFoundPage />, path: "*" },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
