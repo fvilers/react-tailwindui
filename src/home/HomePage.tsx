e537e1d24adc6818531b9160be2649198ef4d221
Fabian Vilers <fabian.vilers@dev-one.com>
2024-06-14 13:50:18 +0200

    Add style to home and not found pages

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,9 +1,18 @@
 function HomePage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">React Tailwind UI</h1>
-      <h2>Beautifully designed, expertly crafted React components</h2>
-    </div>
+    <main className="p-4">
+      <div className="lg:flex lg:items-center lg:justify-between">
+        <div className="min-w-0 flex-1">
+          <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+            React Tailwind UI
+          </h1>
+
+          <p className="mt-2 flex items-center text-sm text-gray-500">
+            Beautifully designed, expertly crafted React components
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
