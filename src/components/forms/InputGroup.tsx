718826db6da63d7d9c60e77b1f6faeeef46dd953
Fabian Vilers <fabian.vilers@dev-one.com>
2024-06-17 11:04:05 +0200

    Add the disabled state to input groups

--- src/components/forms/InputGroup.tsx
+++ src/components/forms/InputGroup.tsx
@@ -28,7 +28,7 @@ function InputGroup({ className, error, help, id, label, ...rest }: Props) {
         <input
           aria-describedby={error ? errorId : descriptionId}
           className={twMerge(
-            "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+            "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6",
             error &&
               "pr-10 text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500",
             className,
